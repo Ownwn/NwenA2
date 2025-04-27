ba2528a6420f42d345494b8075f3b392ac4c4f72
Owen <owen@ownwn.com>
2025-04-28 11:06:47 +1200

    trying

--- server.c
+++ server.c
@@ -1,38 +1,68 @@
-/**
- * Skeleton file for server.c
- * 
- * You are free to modify this file to implement the server specifications
- * as detailed in Assignment 3 handout.
- * 
- * As a matter of good programming habit, you should break up your imple-
- * mentation into functions. All these functions should contained in this
- * file as you are only allowed to submit this file.
- */ 
 
-#include <stdio.h>
-// Include necessary header files
 
-/**
- * The main function should be able to accept a command-line argument
- * argv[0]: program name
- * argv[1]: port number
- * 
- * Read the assignment handout for more details about the server program
- * design specifications.
- */ 
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        return -1;
-    }
-    int port;
 
-    sscanf(argv[1], "%d", &port);
 
-    if (!port || port < 1024 || port > 65536) {
-        return -1;
-    }
-    printf("port is %d\n", port);
-    return 0;
+
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#define SERVER_PORT 12345
+void error(const char * msg){
+printf("Error : %s\n",msg);
+exit(1);
 }
-
+int main(){
+//step 1 create a socket
+int fd = socket(AF_INET, SOCK_STREAM,0);
+if(fd == -1){
+error("Error creating socket");
+}
+printf("Sockt created\n");
+//step 2(a) Create an address and bind socket to it
+struct sockaddr_in serveraddr;
+serveraddr.sin_family = AF_INET;
+serveraddr.sin_port = htons(SERVER_PORT);
+serveraddr.sin_addr.s_addr = INADDR_ANY;
+printf("Address created\n");
+//Step 2(b)bind address to socket
+int ret;
+ret = bind(fd,(struct sockaddr *)&serveraddr,sizeof(serveraddr));
+if(ret ==-1){
+error("Error binding\n");
+}
+printf("Bind successful\n");
+//step 3 listen
+if(listen(fd,SOMAXCONN)<0){
+error("Listening error");
+}
+printf("Listen successful\n");
+//Step 4 accept client connection
+struct sockaddr_in client_addr;
+int addrlen = sizeof(client_addr);
+int client_fd = accept(fd,(struct sockaddr *)&client_addr,
+(socklen_t*)&addrlen);
+if(client_fd<0){
+error("Error accepting client");
+}
+printf("Accept successful\n");
+//step 5 Read and write
+char buffer[100];
+memset(buffer,0,100);
+strncpy(buffer,"Hello from server\n",strlen("Hello from server\n"));
+//int len = send(client_fd,buffer,100,0);
+int len = write(client_fd, buffer,100);
+if(len<0){
+error("Error writing to socket");
+}
+printf("Write success\n");
+memset(buffer,0,100);
+len = read(client_fd, buffer,100);
+if(len<0){
+error("Error reading from client socket");
+}
+printf("Read success. Msg received: %s\n",buffer);
+close(client_fd);
+}
